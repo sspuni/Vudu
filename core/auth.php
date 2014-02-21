3ca19a27081433789a61be44f8ef5f7d0e90c6c0
Sandeep Singh <s4nd33p@gmail.com>
2014-02-21 11:18:39 +0530

    adding comments

--- core/auth.php
+++ core/auth.php
@@ -1,15 +1,37 @@
 <?php
 
-/*
- * Name : auth.php
- * Author : Sandeep Singh
- * Description : AuthController is responsible for handling authorization logic.
+/**
+ * AuthController is responsible for handling authorization logic.
+ * 
+ * This class handles the logic of creating auth sessions. It automatically checks for **authkey** variable in user request and initialize the session accordingly.
+ * 
+ * @class AuthController
+ * @filesource auth.php
+ * @author Sandeep Singh (s4nd33p@gmail.com) 
  */
 
 class AuthController {
+    
+    /**
+     * Tells if the user is authorized or not
+     * @var boolean 
+     */
     private $isAuthorized = false;
+    
+    /**
+     * Holds the authorization info.
+     * 
+     * This is the custom application set information, which is set by using makeAuth() function of this class. It can hold anything, as per the application logic and needs.
+     * @var mixed 
+     */
     private $authInfo = null;
     
+    
+    /**
+     * Class constructor to automatically reset the session and check for authentication information regarding present user.
+     * 
+     * @function __construct 
+     */
     function __construct() {
         
         $this->reset();
--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <?php
 
-/*
- * Name : index.php
- * Author : Sandeep Singh
+/**
+ * Starting point!
+ * 
+ * This is the starting point of this web services framework. Its the only file which needs to be exposed to web by placing it inside some web-accessible directory.
+ * 
+ * @filesource index.php
+ */
+
+/**
+ * Include the one and only main file, required for the whole configuration and running of the framework. 
  */
 
 include 'include/vudu.php';
+
+
+/**
+ * Start the magic! 
+ */
 
 Vudu::run();
 
