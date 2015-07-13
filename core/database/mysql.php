8439f5e7aa50f4fb1450826ffde1fa54817d45a6
Sandeep Singh <sandeep.singh.pooni@gmail.com>
2015-07-13 19:11:32 +0530

    Updated functionality

--- core/database/mysql.php
+++ core/database/mysql.php
@@ -1,10 +1,8 @@
 <?php
-// TODO: complete it ... ;-)
-
 
 
 class MySQLDatabase {
-    private $link;
+    private $db;
     
     private $fields="";
     private $table="";
@@ -20,59 +18,149 @@ class MySQLDatabase {
 
     private function connect() {
         global $config;
-        $this->link = mysql_connect($config['DATABASE']['db_host'], $config['DATABASE']['db_user'], $config['DATABASE']['db_pass']);
-        if($this->link && !empty($config['DATABASE']['db_name'])){
-            mysql_select_db($config['DATABASE']['db_name']);
+        $this->db = new mysqli($config['DATABASE']['db_host'], $config['DATABASE']['db_user'], $config['DATABASE']['db_pass'], $config['DATABASE']['db_name']);
+        if($this->db->connect_errno){
+            echo "Failed to connect to MySQL: " . $this->db->connect_error;
         }
+    }
+
+    public function insert_id(){
+        return $this->db->insert_id;
     }
     
     public function escape($str) {
-        return mysql_real_escape_string($str);
+        return $this->db->real_escape_string($str);
     }
     
-    public function query($query) {
-        return mysql_query($query);
+    public function reset($fields=[]){
+        if(empty($fields)) {
+            $this->fields = $this->join = $this->limit = $this->orderby = $this->table = $this->where = "";
+        }else{
+            foreach ($fields as $f){
+                if(isset($this->{$f})){
+                    $this->{$f} = "";
+                }
+            }
+        }
     }
     
-    public function getColumn($query){
+    public function query($query="", $reset=true) {
+        if(empty($query)){
+            $query = "SELECT ".$this->fields." FROM ".$this->table." ".$this->join." WHERE ".$this->where. " ".$this->orderby. " ".$this->limit;
+            if($reset){
+                $this->reset();
+            }
+        }
+        return $this->db->query($query);
+    }
+    
+    public function getColumn($query=""){
         $res = $this->query($query);
-        if($res && mysql_num_rows($res)){
-            $row = mysql_fetch_array($res, MYSQL_NUM);
+        if($res && $res->num_rows){
+            $row = $res->fetch_array(MYSQLI_NUM);
+            $res->free();
             return $row[0];
         }
         return null;
     }
     
-    public function getRow($query){
+    public function getRow($query=""){
         $res = $this->query($query);
-        if($res && mysql_num_rows($res)){
-            $row = mysql_fetch_assoc($res);
+        if($res && $res->num_rows){
+            $row = $res->fetch_assoc();
+            $res->free();
             return $row;
         }
         return null;
     }
     
-    public function getAll($query){
+    public function getAll($query="", $indexBy=false){
         $res = $this->query($query);
-        if($res && mysql_num_rows($res)){
+        if($res && $res->num_rows){
             $result = array();
-            while(($row = mysql_fetch_assoc($res))){
-                $result[] = $row;
+            while(($row = $res->fetch_assoc())){
+                if($indexBy){
+                    $result[$row[$indexBy]] = $row;
+                }else {
+                    $result[] = $row;
+                }
             }
+            $res->free();
             return $result;
         }
         return null;
     }
     
+    public function tableExists($table){
+        return $this->getRow("SHOW TABLES LIKE '$table'") != null;
+    }
+    
+    public function createTable($table, $fields){
+        $query = "CREATE TABLE IF NOT EXISTS `$table` (";
+        $cols = array();
+        foreach ($fields as $k=>$v){
+            $cols[] = $this->backticks($k)." $v";
+        }
+        $query .= implode(",", $cols);
+        $query .= ");";
+        return $this->query($query);
+    }
+    
+    private function backticks($field){
+        if(preg_match("@^[a-zA-Z0-9_.\s]+$@", $field)) {
+            if(strpos($field, ".")!==FALSE){
+                $parts = explode(".", $field);
+                foreach ($parts as $k=>$v){
+                    $parts[$k] = "`".trim($v)."`";
+                }
+                $field = implode(".", $parts);
+            }else{
+                $field = "`".$field."`";
+            }
+        }
+        return $field;
+    }
 
-    public function insert($table, $data, $escape=true) {
+    public function insert($table, $data, $escape=true, $on_duplicate="") {
         $query = "INSERT INTO `".($escape ? $this->escape($table) : $table)."` SET ";
         $values = array();
         foreach($data as $k=>$v) {
-            if($escape) $values[] = "`".$this->escape($k)."`=".(is_numeric($v) ? $v : ("'".$this->escape($v)."'"));
-            else $values[] = $k."=".(is_numeric($v) ? $v : ("'".$v."'"));
+            if($escape) $values[] = $this->backticks($this->escape($k))."=".(is_numeric($v) ? $v : ("'".$this->escape($v)."'"));
+            else $values[] = $this->backticks($k)."="."'".$v."'"; //(is_numeric($v) ? $v : ("'".$v."'"));
         }
         $query .= implode(',', $values);
+        
+        if(!empty($on_duplicate)){
+            if(is_array($on_duplicate)) {
+                $dvalues = array();
+                foreach($on_duplicate as $k=>$v) {
+                    if($escape) $dvalues[] = $this->backticks($this->escape($k))."=".(is_numeric($v) ? $v : ("'".$this->escape($v)."'"));
+                    else $dvalues[] = $this->backticks($k)."="."'".$v."'"; //(is_numeric($v) ? $v : ("'".$v."'"));
+                }
+                $query .= " ON DUPLICATE KEY UPDATE ".implode(',', $dvalues);
+            }else{
+                $query .= " ON DUPLICATE KEY UPDATE ".$on_duplicate;
+            }
+        }
+        return $this->query($query);
+    }
+    
+    public function update($table, $data, $cond, $joinby='AND', $escape=true) {
+        $query = "UPDATE `".($escape ? $this->escape($table) : $table)."` SET ";
+        
+        $values = array();
+        foreach($data as $k=>$v) {
+            if($escape) $values[] = $this->backticks($this->escape($k))."=".(is_numeric($v) ? $v : ("'".$this->escape($v)."'"));
+            else $values[] = $this->backticks($k)."="."'".$v."'"; //(is_numeric($v) ? $v : ("'".$v."'"));
+        }
+        $query .= implode(',', $values);
+        
+        $conds = array();
+        foreach($cond as $k=>$v) {
+            if($escape) $conds[] = $this->backticks($this->escape($k))."=".(is_numeric($v) ? $v : ("'".$this->escape($v)."'"));
+            else $conds[] = $k."="."'".$v."'"; //(is_numeric($v) ? $v : ("'".$v."'"));
+        }
+        $query .= implode(" $joinby ", $conds);
         return $this->query($query);
     }
     
@@ -80,8 +168,8 @@ class MySQLDatabase {
         if(is_array($fields)){
             $f = array();
             foreach($fields as $k=>$v){
-                if($escape) $f[] = "`".$this->escape($v)."`".(!is_numeric($k) ? ' as '.$this->escape($k) : '');
-                else $f[] = $v.(!is_numeric($k) ? ' as '.$k : '');
+                if($escape) $f[] = $this->backticks($this->escape($v)).(!is_numeric($k) ? ' as '.$this->escape($k) : '');
+                else $f[] = $this->backticks($v).(!is_numeric($k) ? ' as '.$k : '');
             }
             $f = implode(',', $f);
         }else {
@@ -109,21 +197,36 @@ class MySQLDatabase {
         if(is_array($condition)){
             $c = array();
             foreach($condition as $k=>$v){
-                if($escape) $c[] = "`".$this->escape($k)."`=".(is_numeric($v) ? $v : ("'".$this->escape($v)."'"));
-                else $c[] = $k."=".(is_numeric($v) ? $v : ("'".$v."'"));
+                if($escape) $c[] = $this->backticks($this->escape($k))."=".(is_numeric($v) ? $v : ("'".$this->escape($v)."'"));
+                else $c[] = $this->backticks($k)."=".(is_numeric($v) ? $v : ("'".$v."'"));
             }
+            $c = implode(" $joiner ", $c);
         }else {
-            
+            $c = $condition;
         }
+        
+        $this->where = "(". (empty($this->where) ? "$c" : ($this->where . " AND ($c)")) . ")";
+        
     }
     
     public function or_where($condition, $joiner='AND', $braces=true, $escape=true){
+        if(is_array($condition)){
+            $c = array();
+            foreach($condition as $k=>$v){
+                if($escape) $c[] = $this->backticks($this->escape($k))."=".(is_numeric($v) ? $v : ("'".$this->escape($v)."'"));
+                else $c[] = $this->backticks($k)."=".(is_numeric($v) ? $v : ("'".$v."'"));
+            }
+            $c = implode(" $joiner ", $c);
+        }else {
+            $c = $condition;
+        }
         
+        $this->where = "(". (empty($this->where) ? "$c" : ($this->where . " OR ($c)")) . ")";
     }
 
     private function disconnect(){
-        if($this->link){
-            mysql_close($this->link);
+        if($this->db){
+            $this->db->close();
         }
     }
     
@@ -132,6 +235,3 @@ class MySQLDatabase {
     }
     
 }
-
-
-?>
